5760677317f9fcb67b9f7cb13f87edd257859d64
waapsdeveloper <waapsdeveloper@gmail.com>
2024-02-09 19:22:50 +0500

    ffffffff

--- Stripe.js
+++ Stripe.js
@@ -7,29 +7,36 @@ const Stripe_Prebuild_checkout = async () => {
     return new Promise( resolve => {
 
 
-        setTimeout( () => {
-            resolve("BUNKED")
+        setTimeout( async () => {
+
+            let obj = {
+                success_url: `${process.env.REACT_APP_CLIENT_DOMAIN_URL}?success=true`,
+                cancel_url: `${process.env.REACT_APP_CLIENT_DOMAIN_URL}?canceled=true`,
+            }
+            resolve(obj);
+            // const session = await stripe.checkout.sessions.create({
+            //     line_items: [
+            //         {
+            //             price_data: {
+            //                 currency: 'usd',
+            //                 product_data: {
+            //                     name: 'Stubborn Attachments Book',
+            //                 },
+            //                 unit_amount: 2000
+            //             },
+            //             quantity: 1,
+            //         },
+            //     ],
+            //     mode: 'payment',
+            //     success_url: `${process.env.REACT_APP_CLIENT_DOMAIN_URL}?success=true`,
+            //     cancel_url: `${process.env.REACT_APP_CLIENT_DOMAIN_URL}?canceled=true`,
+            // })
+            
+            // resolve({ clientSecret: session.client_secret })
         }, 1000)
     })
 
-    // const session = await stripe.checkout.sessions.create({
-    //     line_items: [
-    //         {
-    //             price_data: {
-    //                 currency: 'usd',
-    //                 product_data: {
-    //                     name: 'Stubborn Attachments Book',
-    //                 },
-    //                 unit_amount: 2000
-    //             },
-    //             quantity: 1,
-    //         },
-    //     ],
-    //     mode: 'payment',
-    //     success_url: `${process.env.REACT_APP_CLIENT_DOMAIN_URL}?success=true`,
-    //     cancel_url: `${process.env.REACT_APP_CLIENT_DOMAIN_URL}?canceled=true`,
-    // })
-    // return { clientSecret: session.client_secret }
+    
 }
 
 module.exports = { Stripe_Prebuild_checkout }
