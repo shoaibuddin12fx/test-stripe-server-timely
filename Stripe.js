e352064aa4e4b5c04274d7c24440771e26399ebd
waapsdeveloper <waapsdeveloper@gmail.com>
2024-02-09 19:41:44 +0500

    urls

--- Stripe.js
+++ Stripe.js
@@ -9,30 +9,30 @@ const Stripe_Prebuild_checkout = async () => {
 
         setTimeout( async () => {
 
-            let obj = {
-                success_url: `${process.env.REACT_APP_CLIENT_DOMAIN_URL}/stripe-checkout-success`,
-                cancel_url: `${process.env.REACT_APP_CLIENT_DOMAIN_URL}/stripe-checkout-failure`,
-            }
-            resolve(obj);
-            // const session = await stripe.checkout.sessions.create({
-            //     line_items: [
-            //         {
-            //             price_data: {
-            //                 currency: 'usd',
-            //                 product_data: {
-            //                     name: 'Stubborn Attachments Book',
-            //                 },
-            //                 unit_amount: 2000
-            //             },
-            //             quantity: 1,
-            //         },
-            //     ],
-            //     mode: 'payment',
-            //     success_url: `${process.env.REACT_APP_CLIENT_DOMAIN_URL}?success=true`,
-            //     cancel_url: `${process.env.REACT_APP_CLIENT_DOMAIN_URL}?canceled=true`,
-            // })
+            // let obj = {
+            //     success_url: `${process.env.REACT_APP_CLIENT_DOMAIN_URL}/stripe-checkout-success`,
+            //     cancel_url: `${process.env.REACT_APP_CLIENT_DOMAIN_URL}/stripe-checkout-failure`,
+            // }
+            // resolve(obj);
+            const session = await stripe.checkout.sessions.create({
+                line_items: [
+                    {
+                        price_data: {
+                            currency: 'usd',
+                            product_data: {
+                                name: 'Stubborn Attachments Book',
+                            },
+                            unit_amount: 2000
+                        },
+                        quantity: 1,
+                    },
+                ],
+                mode: 'payment',
+                success_url: `${process.env.REACT_APP_CLIENT_DOMAIN_URL}?success=true`,
+                cancel_url: `${process.env.REACT_APP_CLIENT_DOMAIN_URL}?canceled=true`,
+            })
             
-            // resolve({ clientSecret: session.client_secret })
+            resolve({ clientSecret: session.client_secret })
         }, 1000)
     })
 
