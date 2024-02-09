902234a097f8d95c3d008701015fa301fab695ca
waapsdeveloper <waapsdeveloper@gmail.com>
2024-02-09 18:25:16 +0500

    custom

--- index.js
+++ index.js
@@ -9,12 +9,32 @@ const express = require('express')
 const app = express()
 
 
-const corsOptions = {
-    origin:"https://localhost:3000",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "Access-Control-Allow-Origin": "*"
-};
-  app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+    var oneof = false;
+    if(req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-method']) {
+        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-headers']) {
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        oneof = true;
+    }
+    if(oneof) {
+        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
+
+    // intercept OPTIONS method
+    if (oneof && req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
 
 app.listen(3001, () => {
     console.log(`Server running at ${process.env.APP_SERVER_DOMAIN_URL}`)
